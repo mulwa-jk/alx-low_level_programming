c4302cc16d217a700c11baaea201efc55d0e0035
mulwa-jk <kiokom.joseph@gmail.com>
2023-02-16 12:53:38 -0800

    C is fun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main-prints "Programming is like building a multilingual puzzle
  *
- * Return :0
+ * return :0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
