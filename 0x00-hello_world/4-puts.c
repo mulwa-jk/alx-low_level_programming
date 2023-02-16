5110454f82ab383d947bd4575c291f7a5d617fe4
mulwa-jk <kiokom.joseph@gmail.com>
2023-02-16 11:57:03 -0800

    C is fun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main-using puts functuion to print a line
+ *int main(void) -using puts functuion to print a line
  *
  * Return value is 0
  */
 int main(void)
-{ 
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);	
 }
