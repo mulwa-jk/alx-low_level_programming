aff5dda7459a73ed76b205076d0aec97ab3e34d1
mulwa-jk <kiokom.joseph@gmail.com>
2023-02-17 01:58:20 -0800

    putchar function

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alpabet++)
+		putchar(alphabte);
+
+	putchar("\n");
+
+	return (0);
+}
