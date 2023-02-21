a4adf0bc6248edff7663288bf7eaa63bdf437cf8
mulwa-jk <kiokom.joseph@gmail.com>
2023-02-21 12:57:01 -0800

    lower or uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for char alphabet
+ * @c: the char to be checked
+ *
+ * Return: 1 if char is lower or upper. else 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
