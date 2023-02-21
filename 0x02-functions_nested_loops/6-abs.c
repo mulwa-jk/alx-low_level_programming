43062389c6db41b02680df4af1b091a2cf88b008
mulwa-jk <kiokom.joseph@gmail.com>
2023-02-21 13:29:45 -0800

    absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - computes the abolute value of a num
+ * @n: the num in question
+ *
+ * Return: the absolute of the num
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
